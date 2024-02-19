1901b8e99fd995d9deb40bab09f89c02bb1be2e1
alexandra_f <avfriesen@mail.ru>
2024-02-19 23:18:14 +0300

    Lesson6 Task1,2 added

---
+++ Lesson6Preprocessors/Task1/Task1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#define MODE 0
+
+#ifndef MODE 
+#error: MODE has to be defined
+#endif
+
+#if MODE == 1
+int add(int x, int y) {
+    return x + y;
+}
+#endif
+
+int main()
+{
+    setlocale(LC_ALL, "Russian"); // задаём русский текст
+#if MODE == 0
+    std::cout << "Работаю в режиме тренировки. \n";
+#elif MODE == 1
+    int x, y;
+
+    std::cout << "Работаю в боевом режиме" << std::endl;
+    std::cout << "Введите число 1: ";
+    std::cin >> x;
+    std::cout << " Введите число 2 :";
+    std::cin >> y;
+    std::cout << "Результат сложения: " << add(x, y) << std::endl;
+#else
+    std::cout << "Неизвестный режим. Завершение работы." << std::endl;
+#endif
+}
+
